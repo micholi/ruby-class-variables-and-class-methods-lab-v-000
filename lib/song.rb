ae448d3297109dff8d670421624d4d22eed3abe9
Michelle Olivieri <michelleo72@gmail.com>
2018-03-26 22:01:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,14 +35,9 @@ attr_accessor :name, :artist, :genre
         genre_count_hash[genre] += 1
       else
         genre_count_hash[genre] = 1
-
-
-
-end
-
-
-end
-genre_count_hash
+      end
+    end
+      genre_count_hash
 end
 
   def self.artist_count
