c08180a912ddafd89a6cc82efde321934b3afb91
Michelle Olivieri <michelleo72@gmail.com>
2018-03-26 21:11:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,12 +30,13 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
     genre_hash = {}
     #genre_hash[@genre] = []
+
     @@genres.each do |genre, x = 0|
-      if genre_hash.include?(genre)
-        x+= 1
-      else
+      if !genre_hash.include?(genre)
         genre_hash[@genre] = []
         genre_hash[@genre] << x
+      else
+        x+= 1
 
       end
 genre_hash
